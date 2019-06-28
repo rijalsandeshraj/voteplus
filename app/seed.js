41b79fb0e08bf6b1d9deb260d3c63a221675720f
Sandesh Rijal <sandesh.rijal@aayulogic.com>
2019-06-28 18:17:33 +0545

    Initial Commit

---
+++ app/seed.js
@@ -0,0 +1,42 @@
+window.Seed = (function () {
+    const submissions = [
+        {
+            id: 1,
+            title: 'Python',
+            description: 'Language of the present and the future.',
+            url: '#',
+            votes: 16,
+            avatar: '../public/images/avatars/daniel.jpg',
+            submissionImage: '../public/images/submissions/image-yellow.png',
+        },
+        {
+            id: 2,
+            title: 'Super Life: The Fantasy Turned True',
+            description: 'What if you could turn your dreams into reality instantly?',
+            url: '#',
+            votes: 11,
+            avatar: '../public/images/avatars/kristy.png',
+            submissionImage: '../public/images/submissions/image-rose.png',
+        },
+        {
+            id: 3,
+            title: 'Uncle Jack',
+            description: 'There\'s a lot to learn from Uncle Jack.',
+            url: '#',
+            votes: 17,
+            avatar: '../public/images/avatars/veronika.jpg',
+            submissionImage: '../public/images/submissions/image-steel.png',
+        },
+        {
+            id: 4,
+            title: 'JavaScript',
+            description: 'Language of the web. Not learning it is not an excuse.',
+            url: '#',
+            votes: 9,
+            avatar: '../public/images/avatars/molly.png',
+            submissionImage: '../public/images/submissions/image-aqua.png',
+        }
+    ];
+
+    return { submissions: submissions };
+}());
